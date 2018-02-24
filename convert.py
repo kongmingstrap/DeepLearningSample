c6fea171570c0a814c45ede69738b9b390ce5fb7
kongmings <kongmings.trap@gmail.com>
2018-02-25 07:09:39 +0900

    Add programs

---
+++ convert.py
@@ -0,0 +1,9 @@
+path = './model.h5'
+
+import coremltools
+coreml_model = coremltools.converters.keras.convert(path,
+        input_names = 'image',
+        image_input_names = 'image',
+        class_labels = 'labels.txt')
+
+coreml_model.save('Food.mlmodel')
